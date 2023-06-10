6f663a14d3b4da9f7e610c8fa868e503ce9de8d7
NaZe0320 <rwt1226@naver.com>
2023-06-11 03:19:44 +0900

    [FEAT] write 파트 ui 구현 완료

--- app/src/main/java/com/nbit/Idear/write/WriteThirdFragment.kt
+++ app/src/main/java/com/nbit/Idear/write/WriteThirdFragment.kt
@@ -1,11 +1,14 @@
 package com.nbit.Idear.write
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.flexbox.FlexDirection
 import com.google.android.flexbox.FlexWrap
 import com.google.android.flexbox.FlexboxLayoutManager
@@ -29,12 +32,26 @@ class WriteThirdFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentWriteThirdBinding.inflate(inflater, container, false)
 
+        hideKeyboard()
+
         binding.btnNext.setOnClickListener {
                 parentFragmentManager.beginTransaction()
                     .add(R.id.fl_write, WriteFourthFragment())
                     .addToBackStack("Write")
                     .commit()
         }
+
+        val itemList = listOf<ProfileData>(
+            ProfileData("깔끔한","ESTJ","반말"),
+            ProfileData("까칠한","INTJ","반말"),
+            ProfileData("귀여운","ENFP","존댓말"),
+        )
+        val adapter = ProfileAdapter(itemList) {
+            //버튼 클릭 (수정하기)
+        }
+        binding.rvProfile.layoutManager = LinearLayoutManager(context)
+        binding.rvProfile.adapter = adapter
+
         return binding.root
     }
 
@@ -43,5 +60,10 @@ class WriteThirdFragment : Fragment() {
         _binding = null
     }
 
+    private fun hideKeyboard() {
+        val inputMethodManager =
+            requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view?.windowToken, 0)
+    }
 
 }
