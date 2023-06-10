6045e745df2860ea124077eb645a710010ea7c28
jung0115 <jungim7490@gmail.com>
2023-06-10 18:59:41 +0900

    [ADD] MyPageActivity 생성

---
+++ app/src/main/java/com/nbit/Idear/mypage/MyPageActivity.kt
@@ -0,0 +1,16 @@
+package com.nbit.Idear.mypage
+
+import android.os.Bundle
+import com.nbit.Idear.BindingActivity
+import com.nbit.Idear.R
+import com.nbit.Idear.databinding.ActivityMyPageBinding
+
+// 마이 페이지
+class MyPageActivity : BindingActivity<ActivityMyPageBinding>(R.layout.activity_my_page) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
