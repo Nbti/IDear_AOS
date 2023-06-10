c728a03f517cd119bd8d2ae85cb27460000ee26a
jung0115 <jungim7490@gmail.com>
2023-06-10 17:48:29 +0900

    [TEST] 주석 삭제

--- app/src/main/java/com/nbit/Idear/MainActivity.kt
+++ app/src/main/java/com/nbit/Idear/MainActivity.kt
@@ -3,7 +3,6 @@ package com.nbit.Idear
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-// test
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
