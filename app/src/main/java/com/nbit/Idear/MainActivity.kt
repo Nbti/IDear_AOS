c5e256b3d61654689c4c7da2c99b7df46be8a161
jung0115 <jungim7490@gmail.com>
2023-06-10 17:07:36 +0900

    jm: push test

--- app/src/main/java/com/nbit/Idear/MainActivity.kt
+++ app/src/main/java/com/nbit/Idear/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nbit.Idear
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// push test
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
